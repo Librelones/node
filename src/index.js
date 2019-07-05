77214b3c3c016a22535762a41e8f10bf1a242e83
Librelones <diogomarton@gmail.com>
2019-07-05 20:02:10 -0300

    LikesController

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const mongoose = require ('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://librelon:marton2029@cluster0-qv9b1.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.coqnnect('mongodb+srv://librelon:marton2029@cluster0-qv9b1.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
 });
 
+
 app.use(require('./routes'));
 
-app.listen(3030);
+app.listen(3000);
 
 
+    
