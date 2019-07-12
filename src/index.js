8ec9f98c15eff3136236bb8346052ee128f7f264
Librelones <diogomarton@gmail.com>
2019-07-11 21:51:51 -0300

    o

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
-mongoose.coqnnect('mongodb+srv://librelon:marton2029@cluster0-qv9b1.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://librelon:marton2029@cluster0-qv9b1.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
 });
 
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))); 
 
 app.use(require('./routes'));
 
 app.listen(3000);
 
 
-    
+        
